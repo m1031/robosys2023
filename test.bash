c4769cddcd7a3e440788ceb120e943997d3bc857
Mahiro Yamoto <ma20031031@icloud.com>
2023-11-29 14:01:20 +0900

    Add a commi

--- test.bash
+++ test.bash
@@ -6,18 +6,18 @@ ng () {
       res=1
 }
 res=0
-  out=$(seq 5 | ./plus)
-  [ "${out}" = 15 ] || ng ${LINENO}
-  out=$(seq 3 | ./plus)
-  [ "${out}" = 6 ] || ng ${LINENO}
-  out=$(echo あ| ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-  out=$(echo a | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-  out=$(echo| ./plus) #空文字
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-  [ "$res" = 0 ] && echo OK
-  exit $res
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+out=$(seq 3 | ./plus)
+[ "${out}" = 6 ] || ng ${LINENO}
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo a | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo| ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK
+exit $res
