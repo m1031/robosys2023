91bb44a0ebf909e11918eb6acdab5436875b3707
Mahiro Yamoto <ma20031031@icloud.com>
2023-11-29 10:58:37 +0900

    Add a commi

--- test.bash
+++ test.bash
@@ -7,12 +7,17 @@ ng () {
 }
 res=0
   out=$(seq 5 | ./plus)
-  [ "${out}" = 15 ] || ng ${LINENO}　
+  [ "${out}" = 15 ] || ng ${LINENO}
+  out=$(seq 3 | ./plus)
+  [ "${out}" = 6 ] || ng ${LINENO}
   out=$(echo あ| ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+  out=$(echo a | ./plus)
   [ "$?" = 1 ]      || ng ${LINENO}
   [ "${out}" = "" ] || ng ${LINENO}
   out=$(echo| ./plus) #空文字
   [ "$?" = 1 ]      || ng ${LINENO}
   [ "${out}" = "" ] || ng ${LINENO}
-[ "$res" = 0 ] && echo OK
+  [ "$res" = 0 ] && echo OK
   exit $res
